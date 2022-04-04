8077bf33cb4379905cebcfa1b78f8fb007b2d5b3
PisOliver <pisoliver@icloud.com>
2022-04-04 15:43:14 +0200

    Fixed the output text, and started designing

--- js/index.js
+++ js/index.js
@@ -27,14 +27,12 @@ function convertCurrency() {
     let toCurrencyvalue = document.getElementById("tocurrency").value;
     let fromCurrency = getKeyByValue(currencies, fromCurrencyvalue);
     let toCurrency = getKeyByValue(currencies, toCurrencyvalue);
-    console.log(fromCurrency);
-    console.log(toCurrency);
     let amount = document.getElementById("amount").value;
     let result = document.getElementById("result");
     fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/'+ fromCurrency + '/'+ toCurrency +'.json').then((response) => response.json()).then((data) => {
         let rate = data[toCurrency];
         let convertedAmount = amount * rate;
-        result.innerHTML = convertedAmount;
+        result.innerHTML = amount + ' ' + fromCurrencyvalue + ' ' + " = " + convertedAmount + ' ' +toCurrencyvalue;
     });
 }
 
